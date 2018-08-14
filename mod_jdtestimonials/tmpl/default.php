a80ceb3faec3382353b71ce98c20db1f78cb6f90
Sumesh577 <35709615+Sumesh577@users.noreply.github.com>
2018-08-14 13:07:24 -0700

    updated testimonials
    updated testimonials

--- mod_jdtestimonials/tmpl/default.php
+++ mod_jdtestimonials/tmpl/default.php
@@ -1,6 +1,14 @@
 <?php
 defined('_JEXEC') or die;
 $items = $params->get('items', []);
+$arrow = $params->get('arrow');
+$bullets = $params->get('bullets');
+$NormalColor1 = $params->get('NormalColor1');
+$NormalColor = $params->get('NormalColor');
+$activeColor = $params->get('activeColor');
+$hoverColor = $params->get('hoverColor');
+
+
 $items = (array) $items;
 $active = TRUE;
 ?>
@@ -15,43 +23,142 @@ $active = TRUE;
    .jd-testimonial-bullets-<?php echo $module->id; ?> li.active{
       background: #000;
    }
+   
+   /* Testimonial Wrapper
+=========================== */
+.testimonial-container-<?php echo $module->id; ?> {
+  overflow: hidden;
+  position: relative; }
+  .testimonial-container-<?php echo $module->id; ?> .slide-content .author {
+    position: relative;
+    padding: 9px 0px 45px 89px; }
+    .testimonial-container-<?php echo $module->id; ?> .slide-content .author .author-img {
+      position: absolute;
+      overflow: hidden;
+      border-radius: 50%;
+      background: #eeeeee;
+      width: 70px;
+      height: 70px;
+      left: 0px;
+      top: 0px; }
+      .testimonial-container-<?php echo $module->id; ?> .slide-content .author .author-img img {
+        width: 100%; }
+    .testimonial-container-<?php echo $module->id; ?> .slide-content .author .author-info {
+      position: relative;
+      display: inline-block; }
+      .testimonial-container-<?php echo $module->id; ?> .slide-content .author .author-info h5 {
+        margin: 0; }
+  .testimonial-container-<?php echo $module->id; ?> .slick-dots {
+    position: static; }
+    .testimonial-container-<?php echo $module->id; ?> .slick-dots li {
+      position: relative;
+      display: inline-block;
+      height: 20px;
+      width: 20px;
+      margin: 0 5px;
+      padding: 0;
+      cursor: pointer; }
+      .testimonial-container-<?php echo $module->id; ?> .slick-dots li button {
+        padding: 0;
+        line-height: 1; }
+        .testimonial-container-<?php echo $module->id; ?> .slick-dots li button:before {
+          color: <?php if($NormalColor1=="defualt") { echo '#1c60ff'; }else {echo $NormalColor1; } ?>;
+          height: 20px; 
+          width: 20px;
+          font-size: 16px;
+          line-height: 1;
+          top: 0;
+    opacity: 1;		  }
+		  .testimonial-container-<?php echo $module->id; ?> .slick-dots li.slick-active button:before, 
+		  .testimonial-container-<?php echo $module->id; ?> .slick-dots li button:hover:before {
+				color: <?php echo $activeColor; ?>;
+		}
+  .testimonial-container-<?php echo $module->id; ?> .slick-prev {
+    left: 5px;
+	}
+  .testimonial-container-<?php echo $module->id; ?> .slick-next {
+    right: 5px;
+	}
+
+	.testimonial-container-<?php echo $module->id; ?> .slick-prev:before, 
+	.testimonial-container-<?php echo $module->id; ?> .slick-next:before{
+		color:<?php echo $NormalColor; ?>
+	}
+	.testimonial-container-<?php echo $module->id; ?> .slick-prev:hover:before, 
+	.testimonial-container-<?php echo $module->id; ?> .slick-next:hover:before{
+		color:<?php echo $hoverColor; ?>
+	}
+	<?php if($params->get('customstyle')=="true"){
+		echo $params->get('customstyle');
+		$nameColor = $params->get('nameColor');
+		$designationColor = $params->get('designationColor');
+		$reviewColor = $params->get('reviewColor');
+		$nameSize = $params->get('nameSize');
+		$designationSize = $params->get('designationSize');
+		$reviewSize = $params->get('reviewSize');
+		$nameColor='defualt';
+	
+	?>
+		.testimonial-container-<?php echo $module->id; ?> .author-info .name{
+			color: <?php  echo $nameColor?>;
+			font-size: <?php  echo $nameSize?>px;
+		}	
+		.testimonial-container-<?php echo $module->id; ?> .author-info{
+			color: <?php  echo $designationColor?>;
+			font-size: <?php  echo $designationSize?>px;
+		}
+
+		.testimonial-container-<?php echo $module->id; ?> .text{
+			color: <?php  echo $reviewColor?>;
+			font-size: <?php  echo $reviewSize?>px;
+		}
+	<?php } ?>
 </style>
-<div id="jdTestimonialCarousel<?php echo $module->id; ?>" class="carousel slide pb-5" data-ride="carousel">
-   <?php $active = true; ?>
-   <div class="carousel-inner">
-      <?php foreach ($items as $index => $item) { ?>
-         <div class="carousel-item<?php echo $active ? ' active' : ''; ?>">
-            <?php $active = false; ?>
-            <div class="text-center">
-               <div class="row">
-                  <div class="col">
-                     <img class="circle" src="<?php echo JURI::root() . $item->thumbnail; ?>" alt="<?php echo $item->author_name; ?>" />
-                  </div>
-               </div>
-               <div class="row mt-4">
-                  <div class="col">
-                     <h4><?php echo $item->author_name; ?></h4>
-                     <h4><small><?php echo $item->author_designation; ?></small></h4>
-                     <p><?php echo $item->content; ?></p>
-                  </div>
-               </div>
-            </div>
-         </div>
-      <?php } ?>
-   </div>
-   <ul class="carousel-indicators jd-testimonial-bullets-<?php echo $module->id; ?>">
-      <?php
-      $slideIndex = 0;
-      $active = true;
-      foreach ($items as $index => $item) {
-         ?>
-         <li title="<?php echo $item->author_name; ?>" data-target="#jdTestimonialCarousel<?php echo $module->id; ?>" data-slide-to="<?php
-         echo $slideIndex;
-         $slideIndex++;
-         ?>" class="list-inline-item <?php
-             echo $active ? 'active' : '';
-             $active = false;
-             ?>"></li>
-          <?php } ?>
-   </ul>
+
+ <div class="container py-5">
+    <div class="testimonial-container-<?php echo $module->id; ?> bg-white shadow-lg px-5 py-4 m-0">
+		<?php foreach($items as $item)  { ?>
+			<div class="slide-content">
+			  <div class="author d-flex">
+					<?php if(!empty($item->author_thumbnail)) { ?>
+						<div class="author-img rounded-circle">
+						  <img src="<?php echo $item->author_thumbnail; ?>" alt="" class="img-fluid">
+						</div>
+					<?php } ?>
+					
+				<?php if(!empty($item->author_name) or !empty($item->author_depart)) { ?>
+					<div class="author-info">
+						<?php if(!empty($item->author_name)) { ?>
+						  <h5 class="name"><?php echo $item->author_name; ?></h5>
+						<?php } ?> 
+						<?php if(!empty($item->author_depart)) { ?>
+						  <div class="author-text"><?php echo $item->author_depart; ?></div>
+						<?php } ?>
+					</div>
+				 <?php } ?>
+			  </div>
+			  <?php if(!empty($item->author_companyReview)) { ?>
+				  <div class="text">
+					<?php echo $item->author_companyReview; ?>
+				  </div>
+			  <?php } ?>
+			</div>
+		<?php } ?>
+    </div>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+ <script>
+	  (function($){
+	  $(function(){
+		  $('.testimonial-container-<?php echo $module->id; ?>').slick({
+				  arrows: <?php echo $arrow; ?>,
+				  dots: <?php echo $bullets; ?>,
+				  infinite: true,
+				  speed: 300,
+				  slidesToShow: 1,
+				  adaptiveHeight: true
+			  });
+	  });
+	})(jQuery);
+</script>
