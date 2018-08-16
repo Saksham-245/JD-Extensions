e048271974fd15b0b4fea330b3bf587de9947f0e
Sumesh577 <35709615+Sumesh577@users.noreply.github.com>
2018-08-16 05:33:36 -0700

    added services module

--- mod_jdservices_showcase/tmpl/default.php
+++ mod_jdservices_showcase/tmpl/default.php
@@ -1,34 +1,55 @@
 <?php
 defined('_JEXEC') or die;
 $services = $params->get('services', []);
-$services = modJDServicesShowcaseHelper::formatGrid($services);
 $showcase_height = $params->get('showcase_height', 'auto');
+$LinkOn = $params->get('LinkOn');
+$showReadMore = $params->get('showReadMore');
+$showReadMoreText = $params->get('showReadMoreText');
 ?>
-<div style="height: <?php echo $showcase_height; ?>" class="row jd-services-showcase mb-4">
-   <?php foreach ($services as $serviceCol) { ?>
-      <div class="col-12 flex-column col-lg-<?php echo 12 / count($services); ?> <?php echo count($serviceCol) > 1 ? 'pb-lg-4' : ''; ?>">
-         <?php foreach ($serviceCol as $service) { ?>
-            <a class="d-flex mb-4 align-items-stretch jd-services-showcase-link <?php echo count($serviceCol) > 1 ? 'h-50' : 'h-100'; ?>" href="<?php echo JRoute::_("index.php?Itemid=" . $service->link); ?>" title="<?php echo $service->title; ?>">
-               <div class="card w-100 border-0 rounded-0 jdss-item" style="background-image:url(<?php echo JURI::root() . $service->thumbnail; ?>)">
-                  <div class="card-body d-flex align-items-center justify-content-center">
-                     <div class="jdss-item-opacity"></div>
-                     <div class="jdss-item-content">
-                        <div class="text-center">
-                           <h5 class="text-light"><?php echo $service->title; ?></h5>
-                           <h6 class="text-light"><small><?php echo $service->subtitle; ?></small></h6>
+<div class="row">
+    <?php foreach ($services as $service) { ?>
+        <div class="col-12 col-md-6 col-lg-4 d-flex mb-5">
+			<?php if($LinkOn =="fullBox") { ?> <a href="<?php echo $url = JRoute::_('index.php?Itemid=' . $service->link); ?>"><?php } ?>
+                <div class="features-box-icon-wrapper w-100 shadow-lg d-flex flex-row icon-left p-4">
+                <?php if(!empty($service->serveices_icon_upload) or !empty($service->serveices_icon_class) ) {?>
+                        <div class="features-box-icon pr-4">
+                            <?php if($service->serveices_iconOption ==  "icon") { ?>
+                                <i class="<?php echo $service->serveices_icon_class; ?>"></i>
+                            <?php }  ?>
+                            <?php if($service->serveices_iconOption ==  "upload") { ?>
+                                <?php if($LinkOn  =="titleAndMedia") { ?> <a href="<?php echo $url = JRoute::_('index.php?Itemid=' . $service->link); ?>"><?php } ?>
+                                    <?php if(!empty($service->serveices_icon_upload)) {?>
+                                        <img src="<?php echo $service->serveices_icon_upload; ?>">
+                                    <?php } ?>
+                                <?php if($LinkOn  =="titleAndMedia") { ?> </a> <?php } ?> 
+                            <?php }  ?>
                         </div>
-                     </div>
-                     <div class="align-items-center justify-content-center jdss-item-overlay bg-light text-primary">
-                        <div class="text-center">
-                           <h5><?php echo $service->title; ?></h5>
-                           <h6><small><?php echo $service->subtitle; ?></small></h6>
-                           <p><?php echo $service->description; ?></p></p>
+                    <?php }  ?>
+                    <?php if(!empty ($service->title) or !empty($service->description)){ ?>
+                        <div class="features-box-content">
+                            <?php if(!empty($service->title)){ ?>
+                                <h4>
+                                    <?php if($LinkOn    =="title" or $LinkOn  =="titleAndMedia") { ?>
+                                        <a href="<?php echo $url = JRoute::_('index.php?Itemid=' . $service->link); ?>">
+                                    <?php } ?>
+                                            <?php echo $service->title; ?>
+                                    <?php if($LinkOn    =="title" or $LinkOn  =="titleAndMedia") { ?>
+                                        </a>
+                                    <?php } ?> 
+                                </h4>
+                            <?php } ?>
+                            <?php if(!empty($service->description)){ ?>
+                                <p><?php echo $service->description; ?></p>
+                            <?php } ?>
+                            <?php if(($showReadMore)){ ?>
+                                <?php if(!empty($showReadMoreText)){ ?>
+                                    <p><a href="<?php echo $url = JRoute::_('index.php?Itemid=' . $service->link); ?>"><?php echo $showReadMoreText; ?></a></p>
+                                <?php } ?>
+                            <?php } ?>
                         </div>
-                     </div>
-                  </div>
-               </div>
-            </a>
-         <?php } ?>
-      </div>
-   <?php } ?>
+                    <?php } ?>
+                </div>
+            <?php if($LinkOn  =="fullBox") { ?></a> <?php } ?> 
+        </div>
+    <?php } ?>
 </div>
