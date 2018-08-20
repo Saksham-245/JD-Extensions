b231c0d951367493891a3e682bc431abd94b02bd
Sumesh577 <35709615+Sumesh577@users.noreply.github.com>
2018-08-20 00:01:39 -0700

    some changes
    some changes

--- mod_accordion/tmpl/default.php
+++ mod_accordion/tmpl/default.php
@@ -11,7 +11,7 @@ $items_accordions = $params->get('items_accordion', []);
   <div class="accordion-box">
     <div class="accordion-header" id="headingOne">
       <h4 class="heading-title-two mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#One<?php echo $module->id.$i;?>" aria-expanded="true" aria-controls="collapseOne">
+        <button class="btn btn-link <?php echo ($i != 1) ? 'collapsed' : ''?>" type="button" data-toggle="collapse" data-target="#One<?php echo $module->id.$i;?>" aria-expanded="true" aria-controls="collapseOne">
           <?php echo  $items_accordion->accordion_panel_title;?>
         </button>
       </h4>
@@ -19,9 +19,11 @@ $items_accordions = $params->get('items_accordion', []);
 
 	<?php if(!empty($items_accordion->accordion_panel_description)) {?>	
 		<div id="One<?php echo $module->id.$i;?>" class="collapse <?php echo ($i==1) ? 'show' : ''?>" aria-labelledby="headingOne" data-parent="#accordionExample">
-		  <div class="accordion-body">
+    <?php if(!empty($items_accordion->accordion_panel_description)) {?> 
+      <div class="accordion-body">
 			 <?php echo  $items_accordion->accordion_panel_description;?>
 		  </div>
+    <?php } ?>  
 		</div>
 	<?php } ?>  
   </div>
