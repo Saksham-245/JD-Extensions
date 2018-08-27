1da8f1e1d6403d00e7d01a843e3acbda1d1adb4a
Sumesh577 <35709615+Sumesh577@users.noreply.github.com>
2018-08-27 05:17:21 -0700

    added packages for jdprofile

---
+++ package/packages/com_jdprofiles/site/layouts/list_layout.php
@@ -0,0 +1,80 @@
+<?php  
+//print_r($displayData); 
+$items = $displayData->get('items');
+$params = $displayData->get('params');
+
+?>
+ <section>
+    <div class="container py-5">
+      <div class="jd-team-showcase-wrapper jd-list-layout-view jd-list-simple-layout">
+        <div class="row">
+          <div class="col-12">
+            <div class="row">
+            <?php foreach($items as $i => $item) { ?>
+              <!-- Team Item wrapper start -->
+              <div class="jd-team-columns col-12">
+                <div class="card-team jd-team-items">
+                  <div class="team-mamber-image-wrapper">
+                    <img src="<?php echo $item->image; ?>" alt="<?php  echo $item->name; ?>" class="team-mamber-image">
+                  </div>
+                  <?php if($params->get('display_name') or $params->get('display_designation') ) { ?>
+                    <div class="card-team-body">
+                      <div class="team-member-content-wrapper">
+                        <?php if($params->get('display_name')) { ?>
+                          <h5 class="team-member-name"><a href="<?php echo JRoute::_('index.php?option=com_jditems&view=item&id='.(int) $item->id); ?>"><?php  echo $item->name; ?></a></h5>
+                        <?php } ?>
+                        <?php if($params->get('display_designation')) { ?>
+                          <?php if(!empty($item->designation)) { ?>
+                            <p class="team-member-designation">
+                              <small><?php  echo $item->designation; ?></small>
+                            </p>
+                          <?php } ?>
+                        <?php } ?>
+                        <?php if(!empty($item->sbio)) { ?>
+                          <p class="team-member-bio"><?php echo $item->sbio;  ?></p>
+                        <?php } ?>    
+                        <ul class="list-unstyled contact-info">
+                          <?php if($params->get('show_Contact')) { ?>
+                            <li>
+                              <i class="fas fa-phone"></i>  <?php  echo $item->phone; ?></li>
+                            <li>
+                          <?php } ?>
+                          <?php if(!empty($item->email)) { ?>
+                            <i class="fas fa-envelope"></i> <?php  echo $item->email; ?></li>
+                          <?php } ?>
+                          <?php if(!empty($item->location)) { ?>
+                          <li>
+                            <i class="fas fa-map-marker-alt"></i>
+                            <?php  echo $item->location; ?></li>
+                          <?php } ?>    
+                        </ul>
+                      </div>
+                    <?php } ?> 
+                    <?php if($params->get('show_socialsIcon')) { ?>
+                        <?php if(!empty($item->social)) { ?>
+                            <div class="social-item-wrapper">
+                              <ul class="social-item <?php echo $params->get('IconStyle');  ?>">
+                                <?php  $socials=  json_decode($item->social);?>
+                                  <?php  foreach($socials as $social){?>
+                                      <li>
+                                          <a href="<?php echo $social->link?>" target="<?php if($params->get('new_tab')){echo '1'; } ?>" >
+                                            <i class="<?php echo $social->icon?>"></i>
+                                          </a>
+                                      </li>
+                                  <?php } ?>
+                              </ul>
+                            </div>
+                          <?php } ?> 
+                        <?php } ?>
+                    <!-- Social item Wrapper End -->
+                  </div>
+                </div>
+              </div>
+            <?php } ?>
+              <!-- Team Item wrapper end -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
---
+++ package/packages/mod_jdprofileshowcase/helper.php
@@ -0,0 +1,30 @@
+
+<?php
+/**
+ * @package	JD profile Showcase
+ * @subpackage  mod_jdprofileshowcase
+ * @version	1.0
+ * @author	Joomdev.com
+ * @copyright	Copyright (C) 2008 - 2018 Joomdev.com. All rights reserved
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+$doc = JFactory::getDocument();
+// Style Sheet
+$doc->addStyleSheet(JURI::root().'media/mod_jdprofileshowcase/assets/css/jd-profile-style.css');
+$doc->addStyleSheet(JURI::root().'media/mod_jdprofileshowcase/assets/css/jd-profile-style.css.map');
+
+class modJdprofileShowcaseHelper {
+    public function  profiles($limit){
+        $db = JFactory::getDBO();
+        $query = $db->getQuery(true);
+        $query->select('*');
+        $query->from('#__jdprofiles_profiles');
+        $query->where('state = 1');
+        $query->order('ordering desc');
+        $query->setLimit($limit);
+        $db->setQuery($query);
+        $results = $db->loadObjectList();
+        return $results;
+    }
+}
