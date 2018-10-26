7904d705427d39d6ffc7388d5f58d090d0a1f75c
Sumesh577 <35709615+Sumesh577@users.noreply.github.com>
2018-10-26 16:41:21 -0700

    Jd profiles to jd profiler

--- jdprofiles_lite/packages/com_jdprofiles/administrator/sql/uninstall.mysql.utf8.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `#__jdprofiles_profiles`;
-DROP TABLE IF EXISTS `#__jdprofiles_designation`;
-DROP TABLE IF EXISTS `#__jdprofiles_team`;
-DROP TABLE IF EXISTS `#__jdprofiles_skill`;
