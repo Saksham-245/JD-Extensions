191b56552c4bad41e62b0011747fc91d69724eb7
Chandan Deep <chandanndeep@gmail.com>
2018-10-26 15:17:41 +0530

    More changes

--- jdprofiles_lite/packages/mod_jdprofileshowcase/helper.php
+++ jdprofiles_lite/packages/mod_jdprofileshowcase/helper.php
@@ -3,8 +3,7 @@
 /**
  * @package	JD profile Showcase
  * @subpackage  mod_jdprofileshowcase
- * @version	1.0
- * @author	Joomdev.com
+ * @author	JoomDev
  * @copyright	Copyright (C) 2008 - 2018 Joomdev.com. All rights reserved
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
