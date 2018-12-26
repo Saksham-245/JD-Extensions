5973f4eaa3b6bd9d2aedb0f1f32af69095546220
Mukeshraj0768 <34932248+Mukeshraj0768@users.noreply.github.com>
2018-12-26 17:22:02 +0530

    changes html and css

--- jd_tourshowcase/packages/com_jdtoursshowcase/site/views/tours/tmpl/default.php
+++ jd_tourshowcase/packages/com_jdtoursshowcase/site/views/tours/tmpl/default.php
@@ -49,47 +49,61 @@ $db = JFactory::getDbo();
 		<tbody>
 		<div class="row">
 		<?php foreach ($this->items as $i => $item) :  ?>
-			<div class="col-lg-4 d-flex">
-				<div class="card">
-				<a href="<?php echo JRoute::_('index.php?option=com_jdtoursshowcase&view=tour&id='.(int) $item->id); ?>">	<img src="<?php echo $item->tour_image; ?>" alt="top-destinations" class="card-img-top img-fluid"></a>
-					<div class="card-body text-center">
-							<a href="<?php echo JRoute::_('index.php?option=com_jdtoursshowcase&view=tour&id='.(int) $item->id); ?>">
-								<h5 class="card-title"></h5>
-									<?php echo $this->escape($item->title); ?>
-								</h5>
-							</a>
-							
-							<p class="card-text"><b>
+			<div class="jd-col-lg-4 d-flex">
+				<div class="tour-wrap">
+					<div class="tour-view-img">
+						<a href="<?php echo JRoute::_('index.php?option=com_jdtoursshowcase&view=tour&id='.(int) $item->id); ?>">	<img src="<?php echo $item->tour_image; ?>" alt="top-destinations" class="card-img-top img-fluid">
+						</a>
+						<div class="tour-discount">
 							<?php
 								if($item->show_discount){
-									if($item->discount_type=="percentage"){
-										echo "<br>";	
-										echo $item->percentage. '%' .' <br> Off';
-										echo "<br>";	
-										$percentage =  (($item->price*$item->percentage)/100);
-										echo '$'.$price =  ($item->price - $percentage);
+									if($item->discount_type=="percentage"){	
+										echo '<div class="off_percentage"><span>'.$item->percentage. '%' .'</span> </br>Off' . '</div>';	
 										
 									}elseif($item->discount_type=="fixed_amount"){
-										echo "<br>";	
-										echo 'Flat '.$item->fixed_amount.' Off';
-										echo "<br>";
-										$fixed_amount = $item->fixed_amount;
-										echo '$'.$price = ($item->price - $fixed_amount);
+										echo '<div class="off_fixed_amount"> <span>Flat '.$item->fixed_amount.' </span></br>Off' .'</div>';
 									}
 								}
 							?>
-							</b> <?php  if($item->show_discount){ ?>   <del>$<?php echo  $item->price; ?></del><br> <?php }  ?>
-							</b> <?php  if(!$item->show_discount){ ?>  $<?php echo  $item->price; ?><br> <?php }  ?>
+						</div>
+					</div>
+					<div class="tour-body text-center">
+						<div class="tour-title">
+							<a href="<?php echo JRoute::_('index.php?option=com_jdtoursshowcase&view=tour&id='.(int) $item->id); ?>">
+								<h5><?php echo $this->escape($item->title); ?></h5>
+							</a>
+						</div>
+						<div class="tour-sub-title">
+							<span class="text-muted"><?php echo $item->tour_type; ?></span>
+						</div>
+							<div class="tour-percentage">
+								<strong>
+									<?php
+										if($item->show_discount){
+											if($item->discount_type=="percentage"){
+												$percentage =  (($item->price*$item->percentage)/100);
+												echo '$'.$price =  ($item->price - $percentage);
+												
+											}elseif($item->discount_type=="fixed_amount"){
+												$fixed_amount = $item->fixed_amount;
+												echo '$'.$price = ($item->price - $fixed_amount);
+											}
+										}
+									?>
+								</strong> 
+								<span><?php  if($item->show_discount){ ?>   <del>$<?php echo  $item->price; ?></del><br> <?php }  ?>
+								<?php  if(!$item->show_discount){ ?>  $<?php echo  $item->price; ?><br> <?php }  ?></span>
+								<p class="tour-person">
 								<?php if(!empty($item->price_postfix)) { ?>
-									<span class="text-muted"><?php echo $item->price_postfix; ?></span>
-									<span class="text-muted"><?php echo $item->tour_type; ?></span>
-								<?php } ?>
-							</p>
-							<p class="card-text">
+										<span class="text-muted"><?php echo $item->price_postfix; ?></span>
+									<?php } ?>
+								</p>
+							</div>
+							<div class="tour-showcase-icon">
 									<?php $features = json_decode($item->feature); foreach($features as $feature){ ?>
 										<i class="<?php echo $feature->icon_class; ?>"  data-toggle="tooltip" data-placement="top" title="<?php echo $feature->tool_tip; ?>" aria-hidden="true"></i>
 									<?php } ?>
-							</p>		
+							</div>		
 							<hr>
 							<a href="<?php echo JRoute::_('index.php?option=com_jdtoursshowcase&view=tour&id='.(int) $item->id); ?>" class="top-destinations-see-more">
 								<b>See More <i class="fa fa-angle-right" aria-hidden="true"></i></b>
